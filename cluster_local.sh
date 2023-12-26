cfdfa337c905bd0a949dba672b30cfa4dbbaf281
shi wb <shiwb@shideMac-mini.local>
2023-12-26 17:34:20 +0800

    bruce: change the file directory bug

--- cluster_local.sh
+++ cluster_local.sh
@@ -1,8 +1,8 @@
 ### build dockerfile
-docker build -t webbshi/evmosd .
+# docker build -t webbshi/evmosd .
 
 
 
 
-rm -rf ./evmos-testnet-wb
-./build/evmosd testnet init-files --v 8 -o ./evmos-testnet-tabi --keyring-backend=test --starting-ip-address 192.167.10.2
+rm -rf ./build/evmos-testnet-wb
+./build/evmosd testnet init-files --v 8 -o ./build/evmos-testnet-tabi --keyring-backend=test --starting-ip-address 192.167.10.2
