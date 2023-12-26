edea40af187f0bcc99d99eedb26abfe6c4b77186
shi wb <shiwb@shideMac-mini.local>
2023-12-26 17:18:08 +0800

    bruce: change the dockerfile

--- cluster_local.sh
+++ cluster_local.sh
@@ -1,2 +1,8 @@
+### build dockerfile
+docker build -t webbshi/evmosd .
 
-./evmosd testnet init-files --v 8 -o /evmos --keyring-backend=test --starting-ip-address 192.167.10.2
+
+
+
+rm -rf ./evmos-testnet-wb
+./build/evmosd testnet init-files --v 8 -o ./evmos-testnet-tabi --keyring-backend=test --starting-ip-address 192.167.10.2
