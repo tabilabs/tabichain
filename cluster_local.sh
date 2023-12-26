2b808a4934a8fc49d5ed545fa310ea8e158fb2db
shi wb <shiwb@shideMac-mini.local>
2023-12-26 18:19:11 +0800

    change the chain-id

--- cluster_local.sh
+++ cluster_local.sh
@@ -5,4 +5,4 @@
 
 
 rm -rf ./build/evmos-testnet-wb
-./build/evmosd testnet init-files --v 8 -o ./build/evmostabi --keyring-backend=test --starting-ip-address 192.167.10.2 --chain-id 9000
+./build/evmosd testnet init-files --v 8 -o ./build/evmostabi --keyring-backend=test --starting-ip-address 192.167.10.2 --chain-id evmos_9000-1
