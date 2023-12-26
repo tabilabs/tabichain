32edacfc582c385148960b4298f98a573856b4f9
shi wb <shiwb@shideMac-mini.local>
2023-12-26 18:52:48 +0800

    bruce debug

--- cluster_local.sh
+++ cluster_local.sh
@@ -1,8 +1,6 @@
 ### build dockerfile
 # docker build -t webbshi/evmosd .
 
-
-
-
 rm -rf ./build/evmos-testnet-wb
+rm -rf /data/evmostabi
 ./build/evmosd testnet init-files --v 8 -o /data/evmostabi --keyring-backend=test --starting-ip-address 192.167.10.2 --chain-id evmos_9000-1
