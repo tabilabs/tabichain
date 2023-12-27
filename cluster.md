01d2ac3d1d043ad0360daa787ea35ee5064f04b0
shi wb <shiwb@shideMac-mini.local>
2023-12-27 14:28:37 +0800

    bruce: change the node url

--- cluster.md
+++ cluster.md
@@ -12,6 +12,11 @@ rm -rf ./chainData/evmostabi
 ./build/evmosd testnet init-files --v 8 -o ./chainData/evmostabi --keyring-backend=test --starting-ip-address 192.167.10.2 --chain-id evmos_9000-1
 ```
 
+change the profile:
+```shell
+
+```
+
 ```shell
 docker-compose -f docker-tabi.yml up -d
 ```
--- cluster_local.sh
+++ cluster_local.sh
@@ -4,3 +4,12 @@
 rm -rf ./build/evmos-testnet-wb
 rm -rf ./chainData/evmostabi
 ./build/evmosd testnet init-files --v 8 -o ./chainData/evmostabi --keyring-backend=test --starting-ip-address 192.167.10.2 --chain-id evmos_9000-1
+
+  for i in {0..7}; do
+    APP_TOML="./chainData/evmostabi/node$i/evmosd/config/app.toml"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' 's/127.0.0.1/0.0.0.0/g' "$APP_TOML"
+    else
+        sed -i 's/127.0.0.1/0.0.0.0/g' "$APP_TOML"
+    fi
+  done
